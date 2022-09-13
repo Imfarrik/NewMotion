9cb321e6243fb3dd427a8c40015bc3cfb96639fe
Imfarrik <farrik1998@mail.ru>
2022-09-13 14:49:04 +0500

    structure fixed

---
+++ app/src/main/java/com/example/myapplicationnewmotion/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.myapplicationnewmotion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplicationnewmotion.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val item: List<String> = (0..100).map { it.toString() }
+        val myAdapter = MyAdapter(item)
+
+        binding.itemBgRecycle.adapter = myAdapter
+    }
+}
