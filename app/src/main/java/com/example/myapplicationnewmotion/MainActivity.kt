1f107e83dd4b270441a51e338fbcd95d25bc58a0
Imfarrik <farrik1998@mail.ru>
2022-09-15 15:36:55 +0500

    Fixed bugs

--- app/src/main/java/com/example/myapplicationnewmotion/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationnewmotion/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.myapplicationnewmotion
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapplicationnewmotion.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +14,8 @@ class MainActivity : AppCompatActivity() {
         val item: List<String> = (0..6).map { it.toString() }
         val myAdapter = MyAdapter(item)
 
+        binding.itemBgRecycle.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         binding.itemBgRecycle.adapter = myAdapter
     }
 }
