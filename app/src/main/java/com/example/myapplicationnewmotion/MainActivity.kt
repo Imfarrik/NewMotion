e7250af48230efb928e427cda6602803c4ccf9a3
Imfarrik <58667099+Imfarrik@users.noreply.github.com>
2022-09-30 18:20:02 +0500

    исправление стилей, statusbar color и navigationbar color, полноэкранный стиль и insets

--- app/src/main/java/com/example/myapplicationnewmotion/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationnewmotion/MainActivity.kt
@@ -2,16 +2,19 @@ package com.example.myapplicationnewmotion
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.myapplicationnewmotion.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         val b = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         val a = findNavController(R.id.fragmentContainerView)
@@ -20,7 +23,6 @@ class MainActivity : AppCompatActivity() {
 
         binding.bottomNavigationView.background = null
 
-
-
     }
+
 }
