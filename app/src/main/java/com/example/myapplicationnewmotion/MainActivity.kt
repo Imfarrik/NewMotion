836777cc4a9f760e7ba4a360c1c425363e839690
Imfarrik <farrik1998@mail.ru>
2022-10-01 20:03:14 +0500

    last

--- app/src/main/java/com/example/myapplicationnewmotion/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationnewmotion/MainActivity.kt
@@ -1,26 +1,72 @@
 package com.example.myapplicationnewmotion
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.NavOptions
+import androidx.navigation.findNavController
 import com.example.myapplicationnewmotion.databinding.ActivityMainBinding
-import com.example.myapplicationnewmotion.fragments.BottomButtonFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val item: List<String> = (0..6).map { it.toString() }
-        val myAdapter = MyAdapter(item)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
-        binding.itemBgRecycle.layoutManager =
-            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        binding.itemBgRecycle.adapter = myAdapter
+        val bottomNavigationView = binding.bottomNavigationView
+        val a = findNavController(R.id.fragmentContainerView)
 
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.bottom_buttons, BottomButtonFragment.newInstance())
-            .commit()
+        bottomNavigationView.background = null
+
+        val options = NavOptions.Builder()
+            .setLaunchSingleTop(true)
+            .setEnterAnim(androidx.appcompat.R.anim.abc_slide_in_bottom)
+            .setExitAnim(androidx.appcompat.R.anim.abc_slide_in_bottom)
+            .setPopEnterAnim(androidx.appcompat.R.anim.abc_slide_in_bottom)
+            .setPopExitAnim(androidx.appcompat.R.anim.abc_slide_in_bottom)
+            .setPopUpTo(a.graph.startDestinationId, false)
+            .build()
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.mainFragment -> {
+                    a.navigate(R.id.mainFragment, null, options)
+                }
+                R.id.transactionsFragment -> {
+                    a.navigate(R.id.transactionsFragment, null, options)
+                }
+                R.id.payFragment -> {
+                    a.navigate(R.id.payFragment, null, options)
+                }
+                R.id.settingsFragment -> {
+                    a.navigate(R.id.settingsFragment, null, options)
+                }
+            }
+            true
+        }
+
+        bottomNavigationView.setOnNavigationItemReselectedListener {
+            return@setOnNavigationItemReselectedListener
+        }
+
+
     }
+
+//    private fun completelyTransparentStatusBar() {
+//        val w = window
+//        w.setFlags(
+//            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+//            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+//        )
+//    }
+
+
 }
--- app/src/main/java/com/example/myapplicationnewmotion/fragments/BottomButtonFragment.kt
+++ app/src/main/java/com/example/myapplicationnewmotion/fragments/BottomButtonFragment.kt
@@ -1,19 +1,55 @@
 package com.example.myapplicationnewmotion.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.myapplicationnewmotion.R
+import com.example.myapplicationnewmotion.databinding.FragmentBottomButtonBinding
+
 
 class BottomButtonFragment : Fragment() {
+
+    private lateinit var binding: FragmentBottomButtonBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_bottom_button, container, false)
+    ): View {
+        binding = FragmentBottomButtonBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+//        binding.transactionBtnBg.setOnClickListener {
+//            requireActivity().supportFragmentManager.beginTransaction()
+//                .add(R.id.mainActivity, TransactionsFragment.newInstance())
+//                .commitNow()
+//        }
+//
+//        binding.homeBtnBg.setOnClickListener {
+//            requireActivity().supportFragmentManager.beginTransaction()
+//                .replace(R.id.mainActivity, MainFragment.newInstance())
+//                .commitNow()
+//        }
+//
+//        binding.payBtnBg.setOnClickListener {
+//            requireActivity().supportFragmentManager.beginTransaction()
+//                .add(R.id.mainActivity, PayFragment.newInstance())
+//                .commitNow()
+//        }
+//
+//        binding.settingsBtnBg.setOnClickListener {
+//            requireActivity().supportFragmentManager.beginTransaction()
+//                .add(R.id.mainActivity, SettingsFragment.newInstance())
+//                .commitNow()
+//        }
+
     }
 
     companion object {
